aed7b91ef47eac225c493d94a297fcd0d6cee2fb
noirvin <vin.sp97@googlemail.com>
2019-10-05 17:43:08 -0700

    finished virus.py with succesfull pytest

--- virus.py
+++ virus.py
@@ -1,4 +1,4 @@
-class Virus(object):
+class Virus():
     '''Properties and attributes of the virus used in Simulation.'''
 
     def __init__(self, name, repro_rate, mortality_rate):
@@ -10,7 +10,7 @@ class Virus(object):
 def test_virus_instantiation():
     #TODO: Create your own test that models the virus you are working with
     '''Check to make sure that the virus instantiator is working.'''
-    virus = Virus("HIV", 0.8, 0.3)
-    assert virus.name == "HIV"
-    assert virus.repro_rate == 0.8
-    assert virus.mortality_rate == 0.3
+    virus = Virus("Black Plague", 0.1, 0.6)
+    assert virus.name == "Black Plague"
+    assert virus.repro_rate == 0.1
+    assert virus.mortality_rate == 0.6
